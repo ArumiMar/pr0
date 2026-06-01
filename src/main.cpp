203b953581f28f92771f51611f0082d6d1c65295
Arumi Mar <arumimarromero19@gmail.com>
2026-05-31 18:57:10 -0600

    - se resolvio el bloqueo del procesador causado por ejecutar analogRead() dentro del Idle Hook. - se implemento la tarea TaskReadADC para leer el hardware en segundo plano. - se utilizo la variable global 'global_pot_raw' para enviar datos de forma segura. - se limpiaron librerias incompatibles

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,39 @@
+/*el uso de freeRTOS permite gestiornar varias tareas de manera eficiente siempre y cuando se 
+respeten las reglas de diseño. los cambios mas criticos estuvieron en como algunas funcionres 
+requieren acceso excllusivo al hardware, en este caso, la lectura del adc, ya que no podia ejercutarse
+dentro del idle. esto se resolvio usando una tarea aislada y una varible global para compartir la lectura del adc. 
+luego, el uso adecuado de las prioridades, permite que la tarea principal controle los tiempor del 
+led. 
+*/
+//librerias y definicion de harware 
 #include <Arduino.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_freertos_hooks.h"
+#include "freertos/FreeRTOS.h" 
+#include "freertos/task.h" //para crear tareas 
+#include "esp_freertos_hooks.h" //usar el tiempo de inactividad 
 
-#define LED_PIN 2
-#define BUTTON_PIN 33
-#define POT_ADC_CHANNEL 34
+#define LED_PIN 4 // pin para conectar el led externo 
+#define BUTTON_PIN 33 // pin para conectar el boton 
+#define POT_ADC_CHANNEL 34 // pin para la lectura analogica 
 
-TaskHandle_t taskFastHandle = NULL;
-TaskHandle_t taskSlowHandle = NULL;
+TaskHandle_t taskFastHandle = NULL;  //parpaedo rapido
+TaskHandle_t taskSlowHandle = NULL;  //parpadeo lento
 
-volatile bool isSystemIdleWindow = false; 
-TickType_t lastIdlePrintTime = 0;
+volatile bool isSystemIdleWindow = false;  // flag para avisar cuando debe de imprimir en la terminal 
+TickType_t lastIdlePrintTime = 0; 
 
-volatile int global_pot_raw = 0;
+volatile int global_pot_raw = 0; //guargar la lectura del pot 
 
 void TaskReadADC(void *pvParameters) {
     while (true) {
         global_pot_raw = analogRead(POT_ADC_CHANNEL);
-        vTaskDelay(pdMS_TO_TICKS(50)); 
+        vTaskDelay(pdMS_TO_TICKS(50));  //esperar 50 ms antes de la siguiente lectura
     }
 }
 
-bool my_idle_hook(void) {
+bool my_idle_hook(void) {  //esta funcion se ejecuta cada vez que el sistema esta inactivo: cuando no hay tareas listas para ejecutarse
     if (isSystemIdleWindow) {
-        TickType_t currentTime = xTaskGetTickCount();
-        if ((currentTime - lastIdlePrintTime) * portTICK_PERIOD_MS >= 500) {
+        TickType_t currentTime = xTaskGetTickCount(); //revisa la actual, la resta con la ultima vez que imprimio y si pasaron más de 500 milisegundos, entra a ejecutar
+        if ((currentTime - lastIdlePrintTime) * portTICK_PERIOD_MS >= 500) { 
 
             int button_state = digitalRead(BUTTON_PIN);
             bool isPressed = (button_state == LOW); 
@@ -42,8 +50,8 @@ bool my_idle_hook(void) {
     return true; 
 }
 
-void TaskFastBlink(void *pvParameters) {
-    while (true) {
+void TaskFastBlink(void *pvParameters) { //parpadeo rapido 
+    while (true) { 
         digitalWrite(LED_PIN, HIGH);
         vTaskDelay(pdMS_TO_TICKS(100));
         digitalWrite(LED_PIN, LOW);
@@ -51,7 +59,7 @@ void TaskFastBlink(void *pvParameters) {
     }
 }
 
-void TaskSlowBlink(void *pvParameters) {
+void TaskSlowBlink(void *pvParameters) { //parpadeo lento 
     while (true) {
         digitalWrite(LED_PIN, HIGH);
         vTaskDelay(pdMS_TO_TICKS(500));
@@ -90,9 +98,9 @@ void setup() {
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     esp_register_freertos_idle_hook(my_idle_hook);
-
+       
+    //tareas 
     xTaskCreate(TaskReadADC, "TaskADC", 2048, NULL, 1, NULL);
-
     xTaskCreate(TaskFastBlink, "TaskFast", 2048, NULL, 1, &taskFastHandle);
     xTaskCreate(TaskSlowBlink, "TaskSlow", 2048, NULL, 1, &taskSlowHandle);
     xTaskCreate(TaskManager, "TaskManager", 2048, NULL, 2, NULL);
